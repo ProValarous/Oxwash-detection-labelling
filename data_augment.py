c289629f07d8932fe2622703ef47b3decbeb5857
Ahmed Atif <65737137+ProValarous@users.noreply.github.com>
2022-07-08 22:57:58 +0500

    data_augementation.py fixed

--- data_augment.py
+++ data_augment.py
@@ -1,14 +1,13 @@
-import keras
+import tensorflow as tf
 # example of horizontal shift image augmentation
 from numpy import expand_dims
-from keras.preprocessing.image import *
-from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import ImageDataGenerator
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
+
 # load the image
-img = load_img('bird.jpg')
+img = tf.keras.utils.load_img('bird.jpg')
 # convert to numpy array
-data = img_to_array(img)
+data = tf.keras.utils.img_to_array(img)
 # expand dimension to one sample
 samples = expand_dims(data, 0)
 # create image data augmentation generator
@@ -18,12 +17,12 @@ it = datagen.flow(samples, batch_size=1)
 # generate samples and plot
 for i in range(9):
 	# define subplot
-	pyplot.subplot(330 + 1 + i)
+	plt.subplot(330 + 1 + i)
 	# generate batch of images
 	batch = it.next()
 	# convert to unsigned integers for viewing
 	image = batch[0].astype('uint8')
 	# plot raw pixel data
-	pyplot.imshow(image)
+	plt.imshow(image)
 # show the figure
-pyplot.show()
+plt.show()
