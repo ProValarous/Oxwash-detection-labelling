b6696f391e2b3cd916f8b4a1419cbc541dc5beea
Ahmed Atif <65737137+ProValarous@users.noreply.github.com>
2022-07-09 01:05:33 +0500

    new file added

--- data_augment.py
+++ data_augment.py
@@ -4,16 +4,20 @@ from numpy import expand_dims
 from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 
+
+
+
 # load the image
-img = tf.keras.utils.load_img('bird.jpg')
+img = tf.keras.utils.load_img('tag.jpg')
 # convert to numpy array
 data = tf.keras.utils.img_to_array(img)
 # expand dimension to one sample
 samples = expand_dims(data, 0)
 # create image data augmentation generator
-datagen = ImageDataGenerator(width_shift_range=[-200,200])
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(width_shift_range=[-200,200])
 # prepare iterator
 it = datagen.flow(samples, batch_size=1)
+
 # generate samples and plot
 for i in range(9):
 	# define subplot
@@ -26,3 +30,5 @@ for i in range(9):
 	plt.imshow(image)
 # show the figure
 plt.show()
+
+
---
+++ data_augmentationV2.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+def data_augmenter():
+    '''
+    Create a Sequential model composed of 6 layers of preprocessing
+    Returns:
+    tf.keras.Sequential
+    '''
+    data_augmentation = tf.keras.Sequential()
+    data_augmentation.add(tf.keras.layers.RandomFlip('horizontal'))
+    data_augmentation.add(tf.keras.layers.RandomFlip('vertical'))
+    data_augmentation.add(tf.keras.layers.RandomRotation(factor=0.4, fill_mode="wrap"))
+    data_augmentation.add(tf.keras.layers.RandomZoom(0.2,0.2))
+    data_augmentation.add(tf.keras.layers.RandomContrast((0.2,0.75)))
+    data_augmentation.add(tf.keras.layers.RandomBrightness((-0.35,0.35)))
+
+    return data_augmentation
+
+
+# load the image
+img = tf.keras.utils.load_img('tag.jpg')
+# convert to numpy array
+data = tf.keras.utils.img_to_array(img)
+# expand dimension to one sample
+samples = np.expand_dims(data, 0)
+
+# Add the image to a batch.
+image = tf.cast(samples, tf.float32)
+
+data_augmentation = data_augmenter()
+
+plt.figure(figsize=(10, 10))
+for i in range(9):
+  ax = plt.subplot(3, 3, i + 1)
+  augmented_image = data_augmentation(image)
+  plt.imshow(augmented_image[0]/255)
+  plt.axis("on")
+plt.show()
