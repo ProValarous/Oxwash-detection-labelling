142740c75c002982ab8798284063dcce719e287d
Ahmed Atif <65737137+ProValarous@users.noreply.github.com>
2022-07-28 22:43:55 +0500

    Files Added

---
+++ data_augmentationV3.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
+
+def random_invert_img(x, p=0.5):
+  if  tf.random.uniform([]) < p:
+    x = (255-x)
+  else:
+    x
+  return x
+
+def plotImages(images_arr):
+    fig, axes = plt.subplots(3, 3, figsize=(8,8))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+gen = ImageDataGenerator(rescale = 1./255,rotation_range=45,shear_range=0.25,channel_shift_range=10, 
+horizontal_flip=False, brightness_range=[0.1,3.0],fill_mode='reflect',preprocessing_function= (lambda x:random_invert_img(x, 0.5)))
+
+
+image_path = '274.png' 
+
+image = np.expand_dims(plt.imread(image_path),0)*255
+print(image)
+
+print(image.shape)
+
+#plt.imshow(image[0])
+
+aug_iter = gen.flow(image, save_to_dir='new/', save_prefix='aug-image-', save_format='png')
+
+aug_images = [next(aug_iter)[0].astype('float32') for i in range(12)]
+
+
